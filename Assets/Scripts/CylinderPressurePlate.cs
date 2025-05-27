a3c36e16779dcc2248b5c8238fd57f7778be0b59
NikoVidovic <niko171202@gmail.com>
2025-05-27 17:27:02 +0200

    Added a nicer way of finding the last clue

--- Assets/Scripts/CylinderPressurePlate.cs
+++ Assets/Scripts/CylinderPressurePlate.cs
@@ -7,7 +7,19 @@ public class CylinderPressurePlate : MonoBehaviour
     [SerializeField] private string requiredCylinderID = "Activator3";
     public bool isActivated = false;
     public DoorOpen door;
-    [SerializeField] private GameObject minerHat;
+    [SerializeField] private GameObject bucket;
+    [SerializeField] private GameObject showerCap;
+    private Vector3 originalBucketPosition;
+    private Quaternion originalBucketRotation;
+
+    private void Start()
+    {
+        if (bucket != null)
+        {
+            originalBucketPosition = bucket.transform.position;
+            originalBucketRotation = bucket.transform.rotation;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,11 +48,22 @@ public class CylinderPressurePlate : MonoBehaviour
         Debug.Log("Sphere Pressure Plate Activated!");
 
         
-        if (minerHat != null)
+        if (showerCap != null)
         {
-            minerHat.SetActive(true);
+            showerCap.SetActive(true);
         }
         
+
+        if (bucket != null)
+        {
+            Vector3 currentRotation = bucket.transform.eulerAngles;
+            bucket.transform.eulerAngles = new Vector3(-39.73f, -23f, -74.43f);
+
+            // end position = (-9.87, 0.135, 3.16)
+            bucket.transform.position = originalBucketPosition + (new Vector3(0.56f, -0.9f, 0f));
+
+        }
+
 
         // Optional: Visual or gameplay feedback
         GetComponent<Renderer>().material.color = Color.cyan;
@@ -51,10 +74,18 @@ public class CylinderPressurePlate : MonoBehaviour
     {
         isActivated = false;
         Debug.Log("Pressure Plate Deactivated!");
-
-        if (minerHat != null)
+        
+        if (showerCap != null)
         {
-            minerHat.SetActive(false);
+            showerCap.SetActive(false);
+        }
+        
+
+        if (bucket != null)
+        {
+            bucket.transform.position = originalBucketPosition;
+            bucket.transform.rotation = originalBucketRotation;
+
         }
 
         Renderer rend = GetComponent<Renderer>();
